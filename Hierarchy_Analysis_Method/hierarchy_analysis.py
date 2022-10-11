b2d9c4f830bc531a26db23e177b3a8d9abb607e6
Khomiakov7706 <khomiakov7705@gmail.com>
2022-10-11 15:53:34 +0600

    Add files via upload
    Base application. At the time, it has basic Hierarchy analysis method functionality + simple view, that are not connected with each other

---
+++ Hierarchy_Analysis_Method/hierarchy_analysis.py
@@ -0,0 +1,75 @@
+import numpy as np
+from re import T
+import view
+
+def vector(matrix):
+    newMatrix = np.zeros(len(matrix))
+    matSum = np.zeros(len(matrix))
+    for i in range(0, len(matrix)):
+        vect = matrix[:, i]
+        w = vect / vect.sum()
+        for j in range(0, len(matrix)):
+            matSum[j] += w[j] 
+    matSum = matSum / len(matrix)
+    return matSum
+
+def matrix(number, matrType, criterionNumber=0):
+    """Function for creating a pairwise matrix"""
+    A = np.ones([number, number])
+    importance = [i for i in range(1, 10)] # list of allowed importance values
+    importance2 = [i/10 for i in range(1, 10)]
+    value = importance + importance2
+    for i in range(0, number):
+        for j in range(0, number):
+            if i < j:
+                if criterionNumber > 0:
+                    a = str(input(f'Compare importance of {matrType} {i+1} to {matrType} {j+1} in criterion {criterionNumber}. \nType an integer from 0.1 to 9: '))
+                else:
+                    a = str(input(f'How much {matrType} {i+1} is more important than {matrType} {j+1}? Type an integer from 0.1 to 9: '))
+                if float(a) in value: # error handling
+                    A[i,j] = float(a)
+                    A[j, i] = 1 / float(a)
+                else:
+                    print("It is an incorrect value. Let's start again \n")
+                    main()
+    return A
+
+def main():
+
+    numberCrit = str(input("How many criteria do you want to enter? Type an integer: "))
+    if numberCrit.isdigit(): # error handling
+        A = matrix(number= int(numberCrit), matrType= 'criterion')
+        print(A)
+        weights = vector(A)
+        for i in range(len(weights)):
+            print(f'Criterion {i} = {np.round(weights[i], 2)}')
+    else:
+        print("Something went wrong. Let's try again \n")
+        main()
+
+    numberAlt = str(input("How many alternatives do you want to enter? Type an integer: "))
+    if numberAlt.isdigit(): # error handling
+        leftTable = np.zeros([int(numberAlt),int(numberCrit)])
+        print(leftTable)
+        for i in range(0, int(numberCrit)):
+            print(f'table  {i+1}' )
+            A = matrix(number= int(numberAlt), matrType='alternative', criterionNumber= i+1)
+            print(A)
+            leftTable[:, i] = vector(A)
+        print()
+        print("Table of indicators of selected alternatives:")
+        print(leftTable)
+    else:
+        print("Something went wrong. Let's try again \n")
+        main()
+    
+    result = np.matmul(leftTable, weights)
+    print()
+    print(result)
+    print()
+    for i in range(len(result)):
+            print(f'Alternative {i} = {np.round(result[i], 2)}')
+
+
+if __name__ == "__main__":
+    main()
---
+++ Hierarchy_Analysis_Method/view.py
@@ -0,0 +1,20 @@
+import PySimpleGUI as sg
+import hierarchy_analysis
+
+layout = [
+    [sg.Text('criteria number\t'), sg.InputText(),
+     sg.Checkbox('input criteria names')
+     ],
+    [sg.Text('alternatives number\t'), sg.InputText(),
+     sg.Checkbox('input alternatives names')
+     ],
+    #[sg.Output(size=(88, 20))],
+    [sg.Submit(), sg.Cancel()]
+]
+window = sg.Window('File Compare', layout)
+event, values = window.read()
+if event in ('Submit'):
+    print(event, values) #debug
+elif event in ('Exit', 'Cancel'):
+    window.close()
+#sg.popup_ok('popup_ok')
